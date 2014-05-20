ac196503fcb375d362abe1a119c5b47fa5c4e42f
Osvaldo Santana Neto <github@osantana.me>
2014-05-20 00:33:21 -0300

    Closes #27: Refactor wsgi.py to copy QS_* wsgi.environ arguments to os.environ

--- project_name/project_name/wsgi.py
+++ project_name/project_name/wsgi.py
@@ -1,14 +1,19 @@
-"""
-WSGI config for newproj project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
+#!/usr/bin/env python
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.production")  # BOOTSTRAP: os.environ.setdefault("DJANGO_SETTINGS_MODULE", "@@name@@.settings.production")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+_application = get_wsgi_application()
+
+
+def application(environ, start_response):
+
+    # Copy all QS_* wsgi environments to os.environ removing QS_ prefix
+    # This is useful to use Apache SetEnv option to pass configuration
+    # arguments to application.
+    for envvar in environ:
+        if envvar.startswith("QS_"):
+            os.environ[envvar[3:]] = environ[envvar]
+
+    return _application(environ, start_response)
