91242360f516b786be1939c28564a4a6b9651bec
Osvaldo Santana Neto <github@osantana.me>
2015-06-17 02:48:19 -0300

    Quickstartup as an external dependency

--- project_name/quickstartup/core/static/widgets/phone.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    "use strict";
-
-    $("input[type=tel]").inputmask("mask", {"mask": "(99) 99999-9999"});
-
-}());
--- project_name/quickstartup/website/urls.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-
-from django.conf.urls import patterns, url
-
-
-urlpatterns = patterns('',
-    url(r"^$", "quickstartup.website.views.index", name="index"),
-)
