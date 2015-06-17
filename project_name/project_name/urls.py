32f21ec2c4cfde63cce240130141bc92ddaa3857
Osvaldo Santana Neto <github@osantana.me>
2015-06-17 00:48:03 -0300

    Move quickstartup related URLs to quickstartup/urls.py module

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,16 +1,15 @@
 # coding: utf-8
 
 
-from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
-from social.backends.utils import load_backends
 
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',  # prefix
+
     # QUICKSTARTUP: Replace the url mapping bellow with your application urls module
     url(r"^", include("apps.sample.urls", namespace="app")),
 
@@ -18,15 +17,5 @@ urlpatterns = patterns('',
     url(r"^admin/", include(admin.site.urls)),
 
     # This url mappings must be the last one
-    url(r"^accounts/", include("quickstartup.accounts.urls", namespace="qs_accounts")),
-    url(r"^contact/$", include("quickstartup.contacts.urls", namespace="qs_contacts")),
-    url(r"^", include("quickstartup.website.urls", namespace="qs_pages")),
+    url(r"^", include("quickstartup.urls")),
 )
-
-# social authentication
-if load_backends(settings.AUTHENTICATION_BACKENDS):
-    urlpatterns += patterns(
-        "",  # prefix
-        url(r"^", include('social.apps.django_app.urls', namespace='social')),
-    )
-
