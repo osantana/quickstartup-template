bcdbc80d5e4e510d4ea65db2e85399d198ed5bdc
Osvaldo Santana Neto <osantana@users.noreply.github.com>
2015-06-29 18:05:42 -0300

    Use new urlpatterns definition format

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -4,11 +4,9 @@
 from django.conf.urls import patterns, include, url
 
 
-urlpatterns = patterns(
-    '',  # prefix
-
+urlpatterns = [
     url(r"^", include("apps.sample.urls", namespace="app")),
 
     # Quickstartup: This url mapping must be the last one
     url(r"^", include("quickstartup.urls")),
-)
+]
